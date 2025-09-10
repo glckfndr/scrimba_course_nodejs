44f7fe66e1566934aa5fe51334ed0b6c8d2e49ce
glckfndr <obulan65@gmail.com>
2025-09-10 11:20:13 +0300

    feat: add CORS in sendJSONResponse

--- utils/sendJSONResponse.js
+++ utils/sendJSONResponse.js
@@ -1,5 +1,8 @@
 export function sendJSONResponse(res, statusCode, data) {
   res.setHeader("Content-Type", "application/json");
+  // Allow CORS for all origins (you can restrict this in production)
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
   res.statusCode = statusCode;
   res.end(JSON.stringify(data));
 }
